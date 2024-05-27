93ac22007c81d2b4f71a84512a62beb4c19beba7
yidyetebeje <yidnekachewtebeje@gmail.com>
2024-05-27 06:36:28 -0400

    final updat

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
 import { getContests, insertStudents } from "@/db/queries";
 import { NextRequest } from "next/server";
-
+export const dynamic = true;
 export async function GET(req: NextRequest) {
   const contests = await getContests();
   return Response.json(contests);
